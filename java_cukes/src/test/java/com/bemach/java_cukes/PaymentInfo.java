2d8a5d803da973aa9760bf6ea2cbabf1a30370f1
drtran <ktran@bemach.com>
2014-09-01 06:52:50 -0400

    Added code for steps in adopting puppies

---
+++ java_cukes/src/test/java/com/bemach/java_cukes/PaymentInfo.java
@@ -0,0 +1,33 @@
+package com.bemach.java_cukes;
+
+public class PaymentInfo {
+	private String name;
+	private String email;
+	private String address;
+	private String pay_type;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPay_type() {
+		return pay_type;
+	}
+	public void setPay_type(String pay_type) {
+		this.pay_type = pay_type;
+	}
+
+}
--- java_cukes/src/test/java/com/bemach/java_cukes/adopting_puppies_steps.java
+++ java_cukes/src/test/java/com/bemach/java_cukes/adopting_puppies_steps.java
@@ -1,12 +1,122 @@
 package com.bemach.java_cukes;
 
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class adopting_puppies_steps {
+	private WebDriver driver;
+	
+	@Before
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+	
 	@Given("^I am on the puppy adoption site$")
 	public void I_am_on_the_puppy_adoption_site() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+		driver.get("http://puppies.herokuapp.com");
+	}
+	
+	@When("^I click the View Details button for \"([^\"]*)\"$")
+	public void I_click_the_View_Details_button_for(String petName) throws Throwable {
+		List<WebElement> names = driver.findElements(By.xpath("//div[@class='name']"));
+		List<WebElement> values = driver.findElements(By.xpath("//input[@value='View Details']"));
+		
+		int index = 0;
+		for (WebElement name: names) {
+			if (name.getText().equals(petName)) {
+				values.get(index).click();
+				break;
+			}
+			index++;
+		}
+	}
+
+	@When("^I click the Adopt Me! button$")
+	public void I_click_the_Adopt_Me_button() throws Throwable {
+		WebElement adoptMe = driver.findElement(By.xpath("//input[@value='Adopt Me!']"));
+		adoptMe.click();
+	}
+
+	private void I_click_the_Adopt_Another_Puppy_button() throws Throwable {
+		WebElement adoptMore = driver.findElement(By.xpath("//input[@value='Adopt Another Puppy']"));
+		adoptMore.click();
+	}
+
+	
+	private boolean isLastPuppy(List<String> puppies, int index) {
+		return index == puppies.size()-1;
+	}
+	
+	@When("^I adopt multiple puppies$")
+	public void I_adopt_multiple_puppies(List<String> puppies) throws Throwable {
+		for (int index = 0; index < puppies.size(); index++) {
+			I_click_the_View_Details_button_for(puppies.get(index));
+			I_click_the_Adopt_Me_button();
+			if (!isLastPuppy(puppies, index)) {
+				I_click_the_Adopt_Another_Puppy_button();
+			}
+		}
+	}
+
+	
+	@When("^I click the Complete the Adoption button$")
+	public void I_click_the_Complete_the_Adoption_button() throws Throwable {
+		WebElement completeTheAdoption = driver.findElement(By.xpath("//input[@value='Complete the Adoption']"));
+		completeTheAdoption.click();
+	}
+
+	@When("^I complete the adoption with$")
+	public void I_complete_the_adoption_with(List<PaymentInfo> piList) throws Throwable {
+		PaymentInfo pi = piList.get(0);
+		WebElement orderName = driver.findElement(By.xpath("//input[@id='order_name']"));
+		orderName.sendKeys(pi.getName());
+		 
+		WebElement orderAddr = driver.findElement(By.xpath("//textarea[@id='order_address']"));
+		orderAddr.sendKeys(pi.getAddress());
+
+		WebElement orderEmail = driver.findElement(By.xpath("//input[@id='order_email']"));
+		orderEmail.sendKeys(pi.getEmail());
+
+		WebElement select = driver.findElement(By.xpath("//select[@id='order_pay_type']"));
+		Select dropDown = new Select(select);
+		List<WebElement> options = dropDown.getOptions();
+		for (WebElement option: options) {
+			if (option.getText().equals(pi.getPay_type())) {
+				option.click();
+				break;
+			}
+		}
+		
+		WebElement commit = driver.findElement(By.xpath("//input[@name='commit']"));
+		commit.click();
+	}
+
+	@Then("^I should see \"([^\"]*)\"$")
+	public void I_should_see(String note) throws Throwable {
+		WebElement thankYouNote = driver.findElement(By.xpath("//p[@id='notice']"));
+		if (note.equals(thankYouNote.getText())) {
+			System.out.println("Test was a SUCCESS!");
+		} else {
+			System.out.println("Test was a FAILURE!");
+		}
+	}
+	
+	@After
+	public void destroy() {
+		driver.close();
 	}
 }
