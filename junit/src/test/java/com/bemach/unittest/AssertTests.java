6b6e53ec806ca5e60cfb38c9072b860dec61debc
drtran <ktran@bemach.com>
2015-08-14 09:15:05 -0400

    Refactored AssertTests

--- junit/src/test/java/com/bemach/unittest/AssertTests.java
+++ junit/src/test/java/com/bemach/unittest/AssertTests.java
@@ -2,99 +2,121 @@ package com.bemach.unittest;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.both;
-import static org.junit.matchers.JUnitMatchers.containsString;
-import static org.junit.matchers.JUnitMatchers.everyItem;
-import static org.junit.matchers.JUnitMatchers.hasItems;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.hamcrest.CoreMatchers;
 import org.hamcrest.core.CombinableMatcher;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AssertTests {
 	@Test
-	public void testAssertArrayEquals() {
+	public void shouldAssertArrayOfBytes() {
 		byte[] expected = "trial".getBytes();
 		byte[] actual = "trial".getBytes();
-		org.junit.Assert.assertArrayEquals("failure - byte arrays not same",
-				expected, actual);
+		assertArrayEquals(expected, actual);
 	}
 
 	@Test
-	public void testAssertEquals() {
-		org.junit.Assert.assertEquals("failure - strings are not equal",
-				"text", "text");
+	public void shouldAssertArrayOfStrings() {
+		String[] expected = {"String 1", "String 2"};
+		String[] actual = {"String 1", "String 2"};
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void testAssertTrue() {
+		org.junit.Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void shouldAssertFalse() {
+		assertFalse(false);
 	}
 
 	@Test
-	public void testAssertFalse() {
-		org.junit.Assert.assertFalse("failure - should be false", false);
-	}
-
-	@Test
-	public void testAssertNotNull() {
-		org.junit.Assert.assertNotNull("should not be null", new Object());
+	public void shouldAssertNotNull() {
+		assertNotNull(new Object());
 	}
 
 	@Test
 	public void testAssertNotSame() {
-		org.junit.Assert.assertNotSame("should not be same Object",
-				new Object(), new Object());
+		String expected = "String 1";
+		String actual = "String 2";
+		org.junit.Assert.assertNotSame(expected, actual);
 	}
 
 	@Test
-	public void testAssertNull() {
-		org.junit.Assert.assertNull("should be null", null);
+	public void shouldAssertNull() {
+		assertNull(null);
 	}
 
 	@Test
-	public void testAssertSame() {
-		Integer aNumber = Integer.valueOf(768);
-		org.junit.Assert.assertSame("should be same", aNumber, aNumber);
+	public void shouldAssertSame() {
+		Integer expected = Integer.valueOf(768);
+		Integer actual = Integer.valueOf(768);
+		assertSame(expected, expected);
 	}
-
+	
+	@Test
+	public void shouldAssertNotSame() {
+		Integer expected = Integer.valueOf(768);
+		Integer actual = Integer.valueOf(768);
+		assertNotSame(expected, actual);
+	}
+	
 	// JUnit Matchers assertThat
-	@SuppressWarnings("deprecation")
 	@Test
-	public void testAssertThatBothContainsString() {
-		org.junit.Assert.assertThat("albumen",
-				both(containsString("a")).and(containsString("b")));
+	public void shouldAssertThatBothContainsString() {
+		String expected = "an element";
+		Assert.assertThat(expected, CoreMatchers.both(CoreMatchers.containsString("an"))
+												 .and(CoreMatchers.containsString("ment")));
 	}
 
 	@Test
-	public void testAssertThathasItemsContainsString() {
-		org.junit.Assert.assertThat(Arrays.asList("one", "two", "three"),
-				hasItems("one", "three"));
+	public void shouldAssertThathasItemsContainsString() {
+		List<String> expected = Arrays.asList(new String[] {"String 1", "String 2", "String 3", "String 4"});;
+		String[] actual = {"String 2", "String 4"};
+		Assert.assertThat(expected, CoreMatchers.hasItems(actual));
 	}
 
 	@Test
-	public void testAssertThatEveryItemContainsString() {
-		org.junit.Assert.assertThat(
-				Arrays.asList(new String[] { "fun", "ban", "net" }),
-				everyItem(containsString("n")));
-	}
-
-	// Core Hamcrest Matchers with assertThat
-	@Test
-	public void testAssertThatHamcrestCoreMatchers() {
-		assertThat("good", allOf(equalTo("good"), startsWith("good")));
-		assertThat("good", not(allOf(equalTo("bad"), equalTo("good"))));
-		assertThat("good", anyOf(equalTo("bad"), equalTo("good")));
-		assertThat(
-				7,
-				not(CombinableMatcher.<Integer> either(equalTo(3)).or(
-						equalTo(4))));
-		assertThat(new Object(), not(sameInstance(new Object())));
+	public void shouldAssertThatEveryItemContainsString() {
+		List<String> expected = Arrays.asList(new String[] { "fun", "ban", "net" });
+		assertThat(expected, CoreMatchers.everyItem(CoreMatchers.containsString("n")));
 	}
 
 	@Test
-	public void testAssertTrue() {
-		org.junit.Assert.assertTrue("failure - should be true", true);
+	public void shouldAssertCoreMatchersStrings() {
+		String expected = "Good Morning!";
+		assertThat(expected, allOf(equalTo("Good Morning!"), startsWith("Good")));
+		assertThat(expected, not(allOf(equalTo("bad"), equalTo("Good"))));
+		assertThat(expected, anyOf(equalTo("bad"), endsWith("Morning!")));
+		
+		String expectedString = new String("My String");
+		String actualString = new String("My String");
+		assertThat(expectedString, not(sameInstance(actualString)));
 	}
+
+	@Test
+	public void shouldAssertCoreMatchersIntegers() {
+		Integer expected = Integer.valueOf(7);
+		assertThat(expected, not(CombinableMatcher.<Integer> either(equalTo(3)).or(equalTo(4))));
+	}
+	
+	
 }
